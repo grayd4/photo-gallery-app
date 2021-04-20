0673bc5d318c8855b8d6f6cb3603a7f8fd2af54e
danielquiz1 <danielquiz1@gmail.com>
2021-04-19 21:55:11 -0700

    completed tutorial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # photo-gallery-app
 photo gallery ionic app for practice
+
+following this tutorial, plus however i change it
+https://ionicframework.com/docs/angular/your-first-app
---
+++ photo-gallery/src/app/services/photo.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
+
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
+  private platform: Platform;
+
+  constructor(platform: Platform) { 
+    this.platform = platform;
+   }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  }
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE});
+    this.photos = JSON.parse(photoList.value) || [];
+
+    // Easiest way to detect when running on the web:
+    // "when the platform is NOT hybrid, do this"
+    if (!this.platform.is('hybrid')) {
+      // Display the photo by rading into base64 format
+      // Display the pjoto by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
+
+        // Web platofrm only: load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }
+  }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+
+    // Convert photo to base 64 formatm required by filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    //Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+    
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webpath to display the new image instead of base64 since it's already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+    }
+    
+
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // "hybrid" will detect cordova or capacitor
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path
+      });
+
+      return file.data;
+    } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(cameraPhoto.webPath!);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  })
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
